b3dd49d950edb17ef28f0abc918183fbfbc6bc90
Wasiu Bakare <wasiubakare@gmail.com>
2022-05-15 13:06:47 +0100

    initialized header files

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+
+#endif /* SHELL_H */
